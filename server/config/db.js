61d45653d98627b334099130d341c5fa01eb4f34
MH Code <mazheriftikhar1@gmail.com>
2025-01-15 15:06:51 +0500

    15-Jan

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    // Listen for successful connection event
     mongoose.connection.on("connected", () => console.log("Database Connected"));
 
     // Connect to MongoDB using the connection string from environment variables
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("Database connection successful");
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1); // Exit the process if there's a failure
